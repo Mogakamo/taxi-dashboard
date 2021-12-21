6ca8442097282f60626d726ee238b5f47dbfb760
Mogakamo <mogaka.amo254@gmail.com>
2021-12-21 12:37:26 +0300

    finished with the Navbar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+import NavBar from './NavBar';
+import Analytics from './Analytics';
+import FAQs from './FAQs';
+import Earnings from './Earnings';
+import Transfers from './Transfers';
+import Profile from './Profile';
 
 function Dashboard() {
     return (
-        <div>
-            
-        </div>
+        <Section>
+            <NavBar />
+            <div className="gird">
+                <div className="row__one">
+                    <Analytics />
+                    <FAQs />
+                </div>
+                <div className="row__two">
+                    <Earnings />
+                    <Transfers />
+                    <Profile />
+                </div>
+            </div>
+        </Section>
     )
 }
+
+const Section = styled.section`
+    margin-left: 18vw;
+    padding: 2rem;
+    height: 100%;
+`;
 
 export default Dashboard
