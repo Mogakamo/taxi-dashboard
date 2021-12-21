e3f44c51eef3c1baff43b96ac637889ce7acd4d7
Mogakamo <mogaka.amo254@gmail.com>
2021-12-21 12:04:31 +0300

    Sidebar is completed..

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -13,6 +13,8 @@ import scrollreveal from "scrollreveal";
 
 
 function SideBar() {
+    const [currentLink, setCurrentLink] = useState(1)
+
     return (
         <>
             <Section>
@@ -24,37 +26,49 @@ function SideBar() {
                     <div className="toggle"></div>
                     <div className="links">
                         <ul>
-                            <li>
+                            <li 
+                                onClick={() => setCurrentLink(1)} 
+                                className={currentLink === 1 ? "active" : ""}>
                                 <a href="#">
                                     <MdSpaceDashboard />
                                     <span>Dashboard</span>
                                 </a>
                             </li>
-                            <li>
+                            <li 
+                                onClick={() => setCurrentLink(2)} 
+                                className={currentLink === 2 ? "active" : ""}>
                                 <a href="#">
                                     <RiDashboard2Fill />
                                     <span>Riders</span>
                                 </a>
                             </li>
-                            <li>
+                            <li 
+                                onClick={() => setCurrentLink(3)}
+                                className={currentLink === 3 ? "active" : ""}>
                                 <a href="#">
                                     <FaAddressCard />
                                     <span>Payment details</span>
                                 </a>
                             </li>
-                            <li>
+                            <li 
+                                onClick={() => setCurrentLink(4)} 
+                                className={currentLink === 4 ? "active" : ""}>
                                 <a href="#">
                                     <GiTwirlCenter />
                                     <span>Learning center</span>
                                 </a>
                             </li>
-                            <li>
+                            <li 
+                                onClick={() => setCurrentLink(5)} 
+                                className={currentLink === 5 ? "active" : ""}>
                                 <a href="#">
                                     <BsFillChatTextFill />
                                     <span>FAQ's</span>
                                 </a>
                             </li>
-                            <li>
+                            <li 
+                                onClick={() => setCurrentLink(6)} 
+                                className={currentLink === 6 ? "active" : ""}>
                                 <a href="#">
                                     <IoSettings />
                                     <span>Settings</span>
@@ -65,13 +79,104 @@ function SideBar() {
                 </div>
                 <div className="logout">
                     <a href="#"><FiLogOut />
-                    <span>Logout</span></a>
+                        <span>Logout</span></a>
                 </div>
             </Section>
         </>
     )
 }
 
-const Section = styled.section``;
+const Section = styled.section`
+    position: fixed;
+    left: 0;
+    background-color: #212121;
+    height: 100vh;
+    width: 18vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2rem 0;
+    gap: 2rem;
+
+    .top {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        width: 100%;
+    }
+
+    .toggle{ 
+        display: none;
+    }
+
+    .brand{ 
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 2rem;
+
+        svg {
+            color: #ffc107;
+            font-size: 2rem;
+        }
+
+        span {
+            font-size: 2rem;
+            color: #ffc107;
+            font-family: 'Permanent Marker', cursive;
+            text-transform: uppercase;
+        }
+    }
+
+    .links {
+        display: flex;
+        justify-content: center;
+        ul {
+            list-style-type: none;
+            flex-direction: column;
+            gap: 1rem;
+            display: flex;
+        }
+        li {
+            padding: 0.6rem 1rem;
+            border-radius: 0.6rem;
+            &:hover {
+                background-color: #ffc107;
+                a {
+                    color: black;
+                }
+            }
+            a {
+                text-decoration: none;
+                display: flex;
+                gap: 1rem;
+                color: white;
+            }
+        }
+        .active {
+            background-color: #ffc107;
+            a {
+                color: black;
+            }
+        }
+    }
+    .logout {
+        padding: 0.3rem 1rem;
+        border-radius: 0.6rem;
+        &:hover {
+            background-color: #da0037;
+        }
+        a {
+            text-decoration: none;
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            color: white;
+        }
+    }
+`;
+
 
 export default SideBar
