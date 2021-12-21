df9685a4e7cdad89b098fb337623e52960d5fec5
Mogakamo <mogaka.amo254@gmail.com>
2021-12-21 12:40:35 +0300

    finished with the dashboard layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -11,7 +11,7 @@ function Dashboard() {
     return (
         <Section>
             <NavBar />
-            <div className="gird">
+            <div className="grid">
                 <div className="row__one">
                     <Analytics />
                     <FAQs />
@@ -30,6 +30,25 @@ const Section = styled.section`
     margin-left: 18vw;
     padding: 2rem;
     height: 100%;
+    .grid {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        gap: 1rem;
+        margin-top: 2rem;
+        .row__one {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            height: 50%;
+            gap: 1rem;
+        } 
+        .row__two {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            height: 50%;
+            gap: 1rem;
+        }
+    }
 `;
 
 export default Dashboard
