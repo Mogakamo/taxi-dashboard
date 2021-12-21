b002c4b01d3d95131865f56fd19b76b9d1b0d3d1
Mogakamo <mogaka.amo254@gmail.com>
2021-12-21 11:22:31 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+import Dashboard from './components/Dashboard'
+import SideBar from './components/SideBar'
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Div>
+      <SideBar />
+      <Dashboard />
+    </Div>
   )
 }
+
+const Div = styled.div``;
 
 export default App
